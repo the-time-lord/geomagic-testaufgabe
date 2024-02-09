50d09b24209498de16257d6ed70ac8545cd4aeae
Daria Katysheva <Daria.Katysheva@Student.HTW-Berlin.de>
2024-02-09 12:54:31 +0100

    Improve the layout of ContactItem

--- src/client/components/ContactItem/ContactItem.jsx
+++ src/client/components/ContactItem/ContactItem.jsx
@@ -1,11 +1,15 @@
+import { Avatar, Stack, Typography } from '@mui/material';
+
 export function ContactItem({ data }) {
   const { name, phone } = data;
 
   return (
-    <div>
-      <h3>{name}</h3>
-      <p>{phone}</p>
-      <br />
-    </div>
+    <Stack direction="row" alignItems="center" spacing={2}>
+      <Avatar>{name.charAt(0)}</Avatar>
+      <Stack direction="column" justifyItems="center">
+        <Typography variant="h6">{name}</Typography>
+        <Typography variant="body2">{phone}</Typography>
+      </Stack>
+    </Stack>
   );
 }
